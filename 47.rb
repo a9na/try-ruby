30c7fe519957bca200515bfbc4d2d263d8793cc3
Ana Novković <anaiscoding@gmail.com>
2024-12-03 17:07:29 +0100

    Create 47.rb

---
+++ 47.rb
@@ -0,0 +1,6 @@
+# Blurb Class Definition
+# You are now creating your own class in Ruby! Classes are blueprints for creating objects that share common properties and behaviors.
+# A class defines the structure and functionality of an object. In this case, you’re creating a "Blurb" class to manage short messages.
+
+class Blurb
+end
