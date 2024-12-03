51abe35ac7966ed8b2b5766351e19b7d0b187e69
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:14:28 +0100

    Create 04.rb

---
+++ 04.rb
@@ -0,0 +1,10 @@
+# Using puts to display the results of multiple formulas on the screen
+
+# Multiplication: 4 times 10
+puts 4 * 10
+
+# Subtraction: 5 minus 12
+puts 5 - 12
+
+# Division: 30 divided by 4 (without puts to observe the behavior)
+30 / 4
