59c8922ab4121df58cb56cd5da796e55e9746b30
Ana Novković <anaiscoding@gmail.com>
2024-12-03 17:09:13 +0100

    Create 49.rb

---
+++ 49.rb
@@ -0,0 +1,11 @@
+# Create a new Blurb object
+blurb1 = Blurb.new
+
+# Display the default output
+puts blurb1  # This will use the to_s method
+
+# Set the content for blurb1
+blurb1.content = "Today Mount Hood Was Stolen!"
+
+# Display the updated Blurb
+puts blurb1  # This will print the updated content, mood, and time
