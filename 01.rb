7d4ea4a6113bf55e099b329e0f635a75224d985c
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:10:01 +0100

    Create 01.rb

---
+++ 01.rb
@@ -0,0 +1,7 @@
+# The number 3 is an Integer object in Ruby
+# The `times` method is called on 3, which will execute the block 3 times
+3.times do
+  # Inside the block, the `print` method outputs the string 'Welcome '
+  # It does not add a newline after the text
+  print 'Welcome '
+end
