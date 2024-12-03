efbc3a7862c1f9ab49847a79226ce6513fb923db
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:22:24 +0100

    Create 12.rb

---
+++ 12.rb
@@ -0,0 +1,8 @@
+# Array of lottery numbers
+lottery_numbers = [12, 47, 35]
+
+# Adding a number to the list (e.g., adding 8)
+lottery_numbers = [12, 47, 35, 8]
+
+# Output the updated list
+puts lottery_numbers
