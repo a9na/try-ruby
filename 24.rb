f1bb42388aa175b6720c06ba03d0525981ab3469
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:33:38 +0100

    Create 24.rb

---
+++ 24.rb
@@ -0,0 +1,12 @@
+# Create an empty hash to store book reviews
+books = {}
+
+# Add book reviews with their ratings
+books["Gravity's Rainbow"] = :splendid
+books["1984"] = :quite_good
+books["Moby Dick"] = :mediocre
+books["Twilight"] = :quite_not_good
+books["Fifty Shades of Grey"] = :abysmal
+
+# Output the books and their ratings
+puts books
