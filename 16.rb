0869e97167fba4c0fd89969be0cd074bf3db7978
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:25:04 +0100

    Create 16.rb

---
+++ 16.rb
@@ -0,0 +1,8 @@
+# Saving the list of lottery numbers in a variable called 'ticket'
+ticket = [12, 47, 35]
+
+# Sorting the 'ticket' array in place (ascending order)
+ticket.sort!
+
+# Output the sorted list
+puts ticket
