16009a31b0297bfcc3f3d9f2f46609247d6785bd
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:25:50 +0100

    Create 17.rb

---
+++ 17.rb
@@ -0,0 +1,7 @@
+# Array of lottery numbers stored in the 'ticket' variable
+ticket = [12, 35, 47]
+
+# Accessing individual elements using square brackets
+puts ticket[0]  # First element
+puts ticket[1]  # Second element
+puts ticket[2]  # Third element
