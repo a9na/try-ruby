79ea80c846242ed37a470eb8e4fc560172b01658
Ana Novković <anaiscoding@gmail.com>
2024-12-03 16:19:58 +0100

    Create 09.rb

---
+++ 09.rb
@@ -0,0 +1,4 @@
+# Trying to reverse the number 40 (this will cause an error)
+40.reverse
+
+#In Ruby, you can call .reverse on a string, but numbers are not strings, so calling .reverse on a number like 40 will result in an error. Ruby does not recognize .reverse as a valid method for numbers.
